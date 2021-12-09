717997798d4d4313073378d8410ad9df53918643
caelmeCj <caelme.cj@gmail.com>
2021-12-10 00:33:27 +0400

    clean up

--- App.js
+++ App.js
@@ -9,23 +9,8 @@
 import React from 'react';
 import type {Node} from 'react';
 import { WebView } from 'react-native-webview';
-import {
-  
-  Text,
-  useColorScheme,
-  View
-} from 'react-native';
-
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <WebView source={{ uri: 'https://google.com/' }} />
