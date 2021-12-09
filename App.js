08a55a422f9e161677f91a35717fd7c9580ddb3e
caelmeCj <caelme.cj@gmail.com>
2021-12-09 23:54:12 +0400

    working webview

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { WebView } from 'react-native-webview';
+import {
+  
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <WebView source={{ uri: 'https://google.com/' }} />
+  );
+};
+
+
+
+export default App;
